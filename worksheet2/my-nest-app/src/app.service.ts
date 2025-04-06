8a30a8d57412b569ebfa38c7404c0037bf52a2c1
TTam122 <tam.thales@gmail.com>
2025-04-06 12:42:23 +1200

    fixing things

--- worksheet2/my-nest-app/src/app.service.ts
+++ worksheet2/my-nest-app/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! <br>Task 19.5 testing';
+    return 'Hello World!';
   }
 }
